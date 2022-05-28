a90f2624cae23315547e5600fcd68dbecd4077b1
Julian L <julian.lawrance@gmail.com>
2022-05-28 11:36:13 +0100

    Improve formatting in stats table

--- src/StatsTable.tsx
+++ src/StatsTable.tsx
@@ -10,18 +10,22 @@ export const StatsTable = (props: StatsTableProps) => {
     const getName = (stat: PokemonStat) => stat.stat.names[0]!.name
 
     return (
-        <Table>
-            <Table.Header>
-                <Table.Row>
-                    {props.stats.map(s => <Table.HeaderCell key={s.id}>{getName(s)}</Table.HeaderCell>)}
-                </Table.Row>
-            </Table.Header>
+        <div className="stats-table">
+            <h4>Stats</h4>
 
-            <Table.Body>
-                <Table.Row>
-                    {props.stats.map(s => <Table.Cell key={s.id}>{s.baseValue}</Table.Cell>)}
-                </Table.Row>
-            </Table.Body>
-        </Table>
+            <Table>
+                <Table.Header>
+                    <Table.Row>
+                        {props.stats.map(s => <Table.HeaderCell key={s.id}>{getName(s)}</Table.HeaderCell>)}
+                    </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                    <Table.Row>
+                        {props.stats.map(s => <Table.Cell key={s.id}>{s.baseValue}</Table.Cell>)}
+                    </Table.Row>
+                </Table.Body>
+            </Table>
+        </div>
     )
 }
