88e83650fb64bf68738dd4e736ae91d20e18f370
Julian L <julian.lawrance@gmail.com>
2022-06-03 00:27:28 +0100

    Fix width of welcome message

--- src/WelcomeMessage.tsx
+++ src/WelcomeMessage.tsx
@@ -1,7 +1,9 @@
 import { Message } from "semantic-ui-react"
 
+import "./WelcomeMessage.css"
+
 export const WelcomeMessage = () => (
-    <div>
+    <div className="welcome-message-container">
         <Message info>
             Select a Pokémon species to view its stats, type effectiveness and moves!
         </Message>
