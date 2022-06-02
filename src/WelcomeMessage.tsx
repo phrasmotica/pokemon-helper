70f75139e3c50cd264225fc1d28c307c2711396b
Julian L <julian.lawrance@gmail.com>
2022-06-03 00:17:04 +0100

    Fix formatting of welcome message on desktop

--- src/WelcomeMessage.tsx
+++ src/WelcomeMessage.tsx
@@ -1,5 +1,9 @@
 import { Message } from "semantic-ui-react"
 
-export const WelcomeMessage = () => <Message info>
-    Select a Pokémon species to view its stats, type effectiveness and moves!
-</Message>
+export const WelcomeMessage = () => (
+    <div>
+        <Message info>
+            Select a Pokémon species to view its stats, type effectiveness and moves!
+        </Message>
+    </div>
+)
